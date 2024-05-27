6f72670616735672cfae66d6f0d71c4cd04a25cc
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-05-27 16:57:24 +0000

    Added week two project

---
+++ project10/scripts.js
@@ -0,0 +1,14 @@
+// const time = new Date().getHours()
+// console.log(time);
+// if (time === 5) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// console.log("helloboyyy");
+document.addEventListener("DOMContentLoaded", () => {
+  const clickBtn = document.getElementById("btn");
+  clickBtn.addEventListener("click", () => {
+    console.log("I was clicked hehe");
+  });
+});
